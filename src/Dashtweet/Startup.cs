ff6201f86e06d94c90e200e4037a321aa38c0c18
Linus Joensson <linus.joensson.ms@outlook.com>
2016-09-12 16:25:55 +0200

    root

--- src/Dashtweet/Startup.cs
+++ src/Dashtweet/Startup.cs
@@ -13,8 +13,6 @@ namespace Dashtweet
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
@@ -23,7 +21,6 @@ namespace Dashtweet
             services.AddSession();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app)
         {
             app.UseSession();
